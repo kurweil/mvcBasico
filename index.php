652659ab11ec5ac17326a48f59a2310cc4c3ea7a
kurweil <fernando@dominio.com>
2015-03-15 13:59:47 +0100

    Tercer commit Modificacion Controller.php Creacion de controlador Index/index

--- index.php
+++ index.php
@@ -14,22 +14,53 @@ if(isset($url[0]))
 }
 if(isset($url[1]))
 {
-    $method = $url[1];
+    if($url[1] != '')
+    {
+        $method = $url[1];
+    }
 }
 if(isset($url[2]))
 {
-    $params = $url[2];
+    if($url[2] != '')
+    {
+        $params = $url[2];
+    }
 }
 
 // En lugar de hacer todo lo de abajo, que tendríamos que hacer en cada archivo (require)
 // Voy a usar un autoload, para que cargue la clase en caso de ser necesaria
 
 spl_autoload_register(function($class){
-    if(file_exists(LIBS.$class.".php")){
-        require LIBS.$class.".php";
+    if(file_exists(LIBS.$class.'.php')){
+        require LIBS.$class.'.php';
     }
 });
 
+$path = './controllers/'.$controller.'.php';
+
+if(file_exists($path))
+{
+    require $path;
+    $controller = new $controller();
+    if(isset($method))
+    {
+        if(method_exists($controller, $method))
+        {
+            // La url estaría completa
+            if(isset($params))
+            {
+                $controller->{$method}($params);
+            }else{
+                $controller->{$method}();
+            }
+        }
+    }else{
+        $controller->index();
+    }
+    //var_dump($controller);
+}else{
+    echo 'Error 404: No se encuentra el archivo';
+}
 
 /*
 require LIBS.'/Session.php';
